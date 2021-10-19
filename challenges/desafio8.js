aead625c4c4f07a890c719f56c525a12ee7f1bd8
Wadson Almeida <wadson.dev92@gmail.com>
2021-10-19 17:42:35 -0300

    Resolve desafio 8

---
+++ challenges/desafio8.js
@@ -0,0 +1,38 @@
+db.air_routes.aggregate(
+  [
+    {
+      $match: {
+        airplane: {
+          $in: ["747", "380"],
+        },
+      },
+    },
+    {
+      $lookup: {
+        from: "air_alliances",
+        localField: "airline.name",
+        foreignField: "airlines",
+        as: "rotas",
+      },
+    },
+    {
+      $unwind: "$rotas",
+    },
+    {
+      $group: {
+        _id: "$rotas.name",
+        totalRotas: {
+          $sum: 1,
+        },
+      },
+    },
+    {
+      $sort: {
+        totalRotas: -1,
+      },
+    },
+    {
+      $limit: 1,
+    },
+  ],
+);
